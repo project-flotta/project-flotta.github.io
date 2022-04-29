7bbd3f6953ad456bff73db3eef5aa34013b623c9
Eloy Coto <eloy.coto@acalustra.com>
2022-04-29 17:51:06 +0200

    Doc layout first changes
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,13 @@
 
 
     // Sticky Menu
-    $(window).scroll(function () {
-        if ($('.navigation').offset().top > 100) {
-            $('.navigation').addClass('nav-bg');
-        } else {
-            $('.navigation').removeClass('nav-bg');
-        }
-    });
+    // $(window).scroll(function () {
+    //     if ($('.navigation').offset().top > 100) {
+    //         $('.navigation').addClass('nav-bg');
+    //     } else {
+    //         $('.navigation').removeClass('nav-bg');
+    //     }
+    // });
 
     // Background-images
     $('[data-background]').each(function () {
